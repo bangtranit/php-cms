269b6c1b2a596173c39c498534c466d6dd26518c
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-07-19 01:34:21 +0900

    Inserting Data into the posts table and displaying it

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,57 +1,64 @@
+<?php include("db_connection.php") ?>
 <div class="col-md-4">
+    <!-- Blog Search Well -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <div class="input-group">
+            <input type="text" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
+        </div>
+        <!-- /.input-group -->
+    </div>
 
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
-                <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
+    <!-- Blog Categories Well -->
+    <div class="well">
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    
+                    <?php 
+                        $query = "SELECT * FROM categories";
+                        $query_result = mysqli_query($connection, $query);
+                        if (!empty($query_result)) {
+                            while ($row = mysqli_fetch_assoc($query_result)) {
+                                $cat_id = $row["id"];
+                                $cat_title = $row["title"];
+                                ?>
+                                <li><a href="#"> <?php echo $cat_title ?> </a>
                                 </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
-
+                                <?php
+                            }
+                        }
+                    ?>
+                </ul>
             </div>
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <div class="well">
+        <h4>Side Widget Well</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    </div>
+
+</div>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php
-    include("includes/header.php")
+    include("includes/header.php");
+    include("includes/db_connection.php");
 ?>
 <body>
 
@@ -22,7 +23,47 @@
                 </h1>
 
                 <!-- First Blog Post -->
-                <h2>
+                <?php 
+                    $query = "SELECT * FROM posts";
+                    $query_result = mysqli_query($connection, $query);
+                    if (!empty($result_query)) {
+                    while ($row = mysqli_fetch_assoc($query_result)) {
+                            $post_title = $row["post_title"];
+                            $post_content = $row["post_content"];
+                            $post_image = $row["post_image"];
+                            $cat_id = $row["cat_id"];
+                            $post_author = $row["post_author"];
+                            $post_date = $row["post_date"];
+                            $post_tags = $row["post_tags"];
+                            $post_comment_count	 = $row["post_comment_count"];
+                            $post_status = $row["post_status"];
+                            $post_user = $row["post_user"];
+                            $post_views_count	 = $row["post_views_count"];
+                        ?>
+                        
+                            <h2>
+                                <a href="#"> <?php echo $post_title  ?> </a>
+                            </h2>
+                            <p class="lead">
+                                by <a href="index.php"><?php echo $post_author  ?> </a>
+                            </p>
+                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date  ?> </p>
+                            <hr>
+                            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                            <hr>
+                            <p> <?php echo $post_content  ?>  </p>
+                            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                            <hr>
+
+                <?php
+                        }
+                    }
+
+                ?>
+
+
+                <!-- <h2>
                     <a href="#">Blog Post Title</a>
                 </h2>
                 <p class="lead">
@@ -35,7 +76,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                <hr> -->
 
                 <!-- Pager -->
                 <?php  include("includes/pagenavi.php") ?>

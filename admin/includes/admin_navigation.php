5cb934ac4cca27ecedfacaec162ddae5cf0a2a9b
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-07-19 21:09:15 +0900

    Creating a HTML table in admin to display a list of posts

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -35,10 +35,10 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts<i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">aaaaa</a>
+                        <a href="posts.php">View all posts</a>
                     </li>
                     <li>
-                        <a href="#">Dropdown Item</a>
+                        <a href="posts.php">Add new post</a>
                     </li>
                 </ul>
             </li>
---
+++ admin/posts.php
@@ -0,0 +1,68 @@
+<?php 
+include("includes/admin_header.php");
+?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/admin_navigation.php") ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                        Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                        
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                    <td>aaaaa</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include("includes/admin_footer.php") ?>
